4138710a10955666bcaf81e4ded7bb666b496bf1
Deepanshu Rana <deepanshurana165@gmail.com>
2020-05-26 11:08:43 +0530

    init(*): Initialized React Typescript Styled-Components Template

---
+++ src/@screens/LandingPage/LandingPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
+
+interface IProps {}
+
+export const LandingPage: React.FC<IProps> = () => <Wrapper>Hello Hey</Wrapper>;
